e16312f516a14d28c1ede23544930e37c2f4a222
EnderKnighterror <106501005+EnderKnighterror@users.noreply.github.com>
2023-10-13 16:52:09 +1030

    code

--- src/main/java/com/example/twouprework/CoinAnimation.java
+++ src/main/java/com/example/twouprework/CoinAnimation.java
@@ -1,15 +1,50 @@
 package com.example.twouprework;
 
 import javafx.animation.RotateTransition;
-import javafx.scene.image.ImageView;
+import javafx.scene.Node;
+import javafx.scene.PointLight;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.transform.Rotate;
 import javafx.util.Duration;
 
 public class CoinAnimation {
 
-    public void animateCoin(ImageView coin, int result, Runnable onFinish) {
+    private final PhongMaterial headMaterial = new PhongMaterial();
+    private final PhongMaterial tailMaterial = new PhongMaterial();
+
+    public CoinAnimation(Image headImage, Image tailImage) {
+        headMaterial.setDiffuseMap(headImage);
+        tailMaterial.setDiffuseMap(tailImage);
+    }
+
+    public Cylinder createCoin() {
+        Cylinder coin = new Cylinder(50, 5);
+        coin.setMaterial(headMaterial);
+        coin.setRotationAxis(Rotate.X_AXIS);
+        return coin;
+    }
+
+    public void animateCoin(Cylinder coin, int result, Runnable onFinish) {
         RotateTransition rt = new RotateTransition(Duration.seconds(1), coin);
-        rt.setByAngle(360);
+        rt.setByAngle(180 * (result + 1));
         rt.setOnFinished(e -> onFinish.run());
         rt.play();
+    }
+
+    public void setCoinSide(Cylinder coin, int result) {
+        coin.setMaterial(result == 0 ? headMaterial : tailMaterial);
+    }
+
+    public PointLight createLight() {
+
+
+        PointLight pointLight = new PointLight(Color.WHITE);
+        pointLight.setTranslateX(0);
+        pointLight.setTranslateY(0);
+        pointLight.setTranslateZ(-500);
+        return pointLight;
     }
 }
--- src/main/java/com/example/twouprework/TwoUpGame.java
+++ src/main/java/com/example/twouprework/TwoUpGame.java
@@ -1,40 +1,79 @@
 package com.example.twouprework;
 import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Cylinder;
+import javafx.scene.shape.Sphere;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class TwoUpGame extends Application {
-    private ImageView coin1;
-    private ImageView coin2;
+    private Cylinder coin1;
+    private Cylinder coin2;
     private Text resultText;
     private GameMechanics gameMechanics = new GameMechanics();
-    private CoinAnimation coinAnimation = new CoinAnimation();
+    private CoinAnimation coinAnimation;
+    private PointLight pointLight;
 
     @Override
     public void start(Stage primaryStage) {
-        VBox root = new VBox(20);
+        coinAnimation = new CoinAnimation(
+                new Image(getClass().getResourceAsStream("/coin_head.png")),
+                new Image(getClass().getResourceAsStream("/coin_tail.png"))
+        );
+
+        StackPane root = new StackPane();
         root.setAlignment(Pos.CENTER);
 
-        coin1 = new ImageView(new Image(getClass().getResourceAsStream("/coin_head.png")));
-        coin2 = new ImageView(new Image(getClass().getResourceAsStream("/coin_head.png")));
+        coin1 = coinAnimation.createCoin();
+        coin1.setTranslateX(-60);
+        coin1.setTranslateY(0);
+        coin1.setTranslateZ(0);
+        coin2 = coinAnimation.createCoin();
+        coin2.setTranslateX(60);
+        coin2.setTranslateY(0);
+        coin2.setTranslateZ(0);
+
+        Sphere testSphere = new Sphere(50);
+        root.getChildren().add(testSphere);
+
+//        coin1 = new Cylinder(50, 5);
+//        coin1.setMaterial(new PhongMaterial(Color.BLUE));
+
+        pointLight = coinAnimation.createLight();
+
+//        Image headImage = new Image(getClass().getResourceAsStream("/coin_head.png"));
+//        ImageView imageView1 = new ImageView(headImage);
+        AmbientLight ambient = new AmbientLight(Color.WHITE);
+        root.getChildren().add(ambient);
+
+
 
         Button flipButton = new Button("Flip Coins");
         flipButton.setOnAction(e -> flipCoins());
 
         resultText = new Text("Bet for Heads-Heads or Tails-Tails and flip the coins");
 
-        root.getChildren().addAll(coin1, coin2, flipButton, resultText);
+        root.getChildren().addAll(coin1, coin2, flipButton, resultText, pointLight);
 
-        Scene scene = new Scene(root, 400, 400);
+        PerspectiveCamera camera = new PerspectiveCamera(true);
+        camera.setTranslateZ(-300);
+
+        Scene scene = new Scene(root, 800, 800, true);
+        scene.setCamera(camera);
+        scene.setFill(Color.GRAY);
+
         primaryStage.setTitle("Two-up Game");
         primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
     private void flipCoins() {
@@ -42,11 +81,11 @@ public class TwoUpGame extends Application {
         int result2 = gameMechanics.flipCoin();
 
         coinAnimation.animateCoin(coin1, result1, () -> {
-            coin1.setImage(new Image(result1 == 0 ? "coin_head.png" : "tails_coin.png"));
+            coinAnimation.setCoinSide(coin1, result1);
         });
 
         coinAnimation.animateCoin(coin2, result2, () -> {
-            coin2.setImage(new Image(result2 == 0 ? "coin_head.png" : "tails_coin.png"));
+            coinAnimation.setCoinSide(coin2, result2);
         });
 
         resultText.setText(gameMechanics.getResultText(result1, result2));
